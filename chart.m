7643db654de6c10dda1632aef5418dccafff9d42
Akshaya Kulasekaran <61582763+Akshu-on-github@users.noreply.github.com>
2023-07-16 19:27:29 +0530

    Add MATLAB plot

---
+++ chart.m
@@ -0,0 +1,13 @@
+x1 = 2018;
+y1 = 2314;
+x2 = 2019;
+y2 = 2090;
+x3 = 2020;
+y3 = 3503;
+x4 = 2021;
+y4 = 4055;
+
+plot(x1, y1, '.', 'MarkerSize', 10);
+plot(x2, y2, '.', 'MarkerSize', 10);
+plot(x3, y3, '.', 'MarkerSize', 10);
+plot(x4, y4, '.', 'MarkerSize', 10);
